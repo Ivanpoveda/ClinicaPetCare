4a9b3ab913557722e3dc9b59549358a1b4129e48
ivanpoveda <ivanpoveda0206@gmail.com>
2025-11-08 20:42:22 -0600

    Se crea repositorio y se realiza un ajuste en el codigo de program.cs

---
+++ ClinicaPetCare/Data/MascotaRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ClinicaPetCare.Models;
+namespace ClinicaPetCare.Data
+{
+    public class MascotaRepository
+    {
+        private readonly List<Mascota> _mascotas = new();
+        private int _nextId = 1;
+
+
+        public void AgregarMascota(Mascota m)
+        {
+            m.Id = _nextId++;
+            _mascotas.Add(m);
+        }
+
+
+        public List<Mascota> ObtenerMascotas()
+        {
+            return _mascotas.ToList();
+        }
+    }
+}
