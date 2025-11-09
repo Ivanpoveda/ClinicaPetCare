f220557861e5127b666cbe3dcfd581c6bddfa070
ivanpoveda <ivanpoveda0206@gmail.com>
2025-11-08 18:16:02 -0600

    Se realiza la creacion de modelo mascota y se realiza una validacion custom

---
+++ ClinicaPetCare/Models/Mascota.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace ClinicaPetCare.Models
+{
+    public class Mascota
+    {
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "El nombre de la mascota es obligatorio")]
+        [MinLength(2, ErrorMessage = "El nombre de la mascota debe tener al menos 2 caracteres")]
+        public string Nombre { get; set; }
+
+
+        [Required(ErrorMessage = "La especie es obligatoria")]
+        public string Especie { get; set; }
+
+
+        public string Raza { get; set; }
+
+
+        [Range(0, 25, ErrorMessage = "La edad debe estar entre 0 y 25 años")]
+        public int Edad { get; set; }
+
+
+        [Required(ErrorMessage = "El nombre del dueño es obligatorio")]
+        [MinLength(3, ErrorMessage = "El nombre del dueño debe tener al menos 3 caracteres")]
+        public string NombreDueno { get; set; }
+
+
+        [Required(ErrorMessage = "El teléfono del dueño es obligatorio")]
+        [RegularExpression(@"^\d{3}-\d{3}-\d{4}$", ErrorMessage = "El teléfono debe tener formato 809-555-1234")]
+        public string TelefonoDueno { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de ingreso")]
+        [CompareNotFutureDate(ErrorMessage = "La fecha de ingreso no puede ser futura")]
+        public DateTime FechaIngreso { get; set; }
+    }
+}
---
+++ ClinicaPetCare/Models/Validators/CompareNotFutureDateAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class CompareNotFutureDateAttribute : ValidationAttribute
+{
+    public override bool IsValid(object value)
+    {
+        if (value == null) return true; // [Required] se encarga si es obligatorio
+        if (value is DateTime dt)
+        {
+            return dt.Date <= DateTime.Now.Date;
+        }
+        return false;
+    }
+}
